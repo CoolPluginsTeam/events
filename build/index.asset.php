8270b78723d1efc2d658c80f81b1a30d383b1d8a
Tarun0423 <taruncoolplugins@gmail.com>
2025-12-19 17:57:31 +0530

    Font family alphabetically

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-date', 'wp-i18n'), 'version' => '570a8d28783c483bcc45');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-date', 'wp-i18n'), 'version' => '3b0833e0c7409e9d9fb2');
