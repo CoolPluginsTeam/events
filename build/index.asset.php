dd55279b0c80911db4ebe38e896e7ea4b9e459ab
Tarun0423 <taruncoolplugins@gmail.com>
2025-12-25 11:08:18 +0530

    Added note readmore button

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'a6e510686c12cb68b0b9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '4b9eb0f0d639fec1b935');
