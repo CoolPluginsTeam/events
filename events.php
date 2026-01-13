8b26a148b2adba68e0419b53abd02da704bd505f
Tarun0423 <taruncoolplugins@gmail.com>
2026-01-13 10:32:24 +0530

    Update main file

--- events.php
+++ events.php
@@ -7,78 +7,143 @@
  * Version:     1.0
  * License:     GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain: evt
+ * Text Domain: events
  */
 
  // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+define( 'EVENTS_VERSION', '1.0.0' );
+define( 'EVENTS_FILE', __FILE__ );
+define( 'EVENTS_PATH', plugin_dir_path( EVENTS_FILE ) );
+define( 'EVENTS_URL', plugin_dir_url( EVENTS_FILE ) );
 
-// Register Block Scripts
-add_action( 'init', function() {
-	$asset_file   = __DIR__ . '/build/index.asset.php';
-	$asset        = file_exists( $asset_file ) ? require_once $asset_file : null;
-	$dependencies = isset( $asset['dependencies'] ) ? $asset['dependencies'] : [];
-	$version      = isset( $asset['version'] ) ? $asset['version'] : filemtime( __DIR__ . '/index.js' );
 
-	// Block JS
-	wp_register_script(
-		'evt-event',
-		plugins_url( 'build/index.js', __FILE__ ),
-		$dependencies,
-		$version,
-		true
-	);
+final class evt_Events_Block {
 
-	// Pass plugin assets URLs to JavaScript
-	wp_localize_script( 'evt-event', 'evtPluginData', [
-		'pluginUrl' => plugins_url( '', __FILE__ ),
-		'images' => [
-			'crazyDJ' => plugins_url( 'assets/images/crazy-DJ-experience-santa-cruz.webp', __FILE__ ),
-			'rockBand' => plugins_url( 'assets/images/cute-girls-rock-band-performance.webp', __FILE__ ),
-			'foodDistribution' => plugins_url( 'assets/images/free-food-distribution-at-mumbai.webp', __FILE__ ),
-		]
-	] );
+	/**
+	 * Plugin instance.
+	 *
+	 * @access private
+	 */
+	private static $instance = null;
 
-	// Block front end style
-	wp_register_style(
-		'evt-event',
-		plugins_url( 'style.css', __FILE__ ),
-		[],
-		filemtime( __DIR__ . '/style.css' )
-	);
+	/**
+	 * Get plugin instance.
+	 *
+	 * @static
+	 */
+	public static function get_instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
 
-	// Block editor style
-	wp_register_style(
-		'evt-event-editor',
-		plugins_url( 'editor.css', __FILE__ ),
-		[],
-		filemtime( __DIR__ . '/editor.css' )
-	);
+	/**
+	 * Constructor.
+	 *
+	 * @access private
+	 */
+	private function __construct() {
+		$this->evt_include_files();
+		register_activation_hook( EVENTS_FILE, array( $this, 'evt_plugin_activate' ) );
+		register_deactivation_hook( EVENTS_FILE, array( $this, 'evt_plugin_deactivate' ) );
+		// Register Block Scripts
+		add_action( 'init', function() {
+			$asset_file   = EVENTS_PATH . '/build/index.asset.php';
+			$asset        = file_exists( $asset_file ) ? require_once $asset_file : null;
+			$dependencies = isset( $asset['dependencies'] ) ? $asset['dependencies'] : [];
+
+			// Block JS
+			wp_register_script(
+				'evt-event',
+				EVENTS_URL . 'build/index.js',
+				$dependencies,
+				EVENTS_VERSION,
+				true
+			);
+
+			// Pass plugin assets URLs to JavaScript
+			wp_localize_script( 'evt-event', 'evtPluginData', [
+				'pluginUrl' => EVENTS_URL,
+				'images' => [
+					'crazyDJ' => EVENTS_URL . 'assets/images/crazy-DJ-experience-santa-cruz.webp',
+					'rockBand' => EVENTS_URL . 'assets/images/cute-girls-rock-band-performance.webp',
+					'foodDistribution' => EVENTS_URL . 'assets/images/free-food-distribution-at-mumbai.webp',
+				]
+			] );
+
+			// Block front end style
+			wp_register_style(
+				'evt-event',
+				EVENTS_URL . 'style.css',
+				[],
+				filemtime( EVENTS_PATH . '/style.css' )
+			);
+
+			// Block editor style
+			wp_register_style(
+				'evt-event-editor',
+				EVENTS_URL . 'editor.css',
+				[],
+				filemtime( EVENTS_PATH . '/editor.css' )
+			);
+			
+			// Fontello Icon Font
+			wp_register_style(
+				'evt-icons',
+				EVENTS_URL . 'assets/css/evt-icons.css',
+				[],
+				filemtime( EVENTS_PATH . 'assets/css/evt-icons.css' )
+			);
+			} );
+
+			// Enqueue Google Fonts (100 fonts including)
+			add_action( 'wp_enqueue_scripts', function() {
+				// Enqueue Icons Icon Font
+				wp_enqueue_style( 'evt-icons' );
+			} );
+
+			add_action( 'enqueue_block_editor_assets', function() {
+				// Enqueue Icons Icon Font for Editor
+				wp_enqueue_style( 'evt-icons' );
+			} );
+	}
 	
-	// Fontello Icon Font
-	wp_register_style(
-		'evt-icons',
-		plugins_url( 'assets/css/evt-icons.css', __FILE__ ),
-		[],
-		filemtime( __DIR__ . '/assets/css/evt-icons.css' )
-	);
-} );
+	/**
+	 * Plugin Activation Hook
+	 * Saves plugin version and install dates
+	 */
+	public function evt_plugin_activate() {
+		update_option( 'events-v', EBEC_VERSION );
+		update_option( 'events_activation_time', date( 'Y-m-d h:i:s' ) );
 
-// Enqueue Google Fonts (100 fonts including)
-add_action( 'wp_enqueue_scripts', function() {
-	// Enqueue Icons Icon Font
-	wp_enqueue_style( 'evt-icons' );
-} );
+		if (!get_option( 'events_initial_save_version' ) ) {
+			add_option( 'events_initial_save_version', EVENTS_VERSION );
+		}
+		if(!get_option( 'events-install-date' ) ) {
+			add_option( 'events-install-date', gmdate('Y-m-d h:i:s') );
+		}
+	}
 
-add_action( 'enqueue_block_editor_assets', function() {
-	// Enqueue Icons Icon Font for Editor
-	wp_enqueue_style( 'evt-icons' );
-} );
+	/**
+	 * Plugin Deactivation Hook
+	 */
+	public function evt_plugin_deactivate() {
+		// Deactivation functionality here (if needed)
+	}
+	/**
+	 * AUTO-GENERATED blocks will be added here
+	 */
 
-/**
- * AUTO-GENERATED blocks will be added here
- */
+	public function evt_include_files() {
+		include_once EVENTS_PATH . '/blocks/event.php';
+	}
 
-include_once __DIR__ . '/blocks/event.php';
+}
+function evt_Events_Block() {
+	return evt_Events_Block::get_instance();
+}
+evt_Events_Block();
